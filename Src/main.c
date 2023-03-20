2f183c16e978618f1876c302ef0bd7f5b84fb69f
Kommandant19 <rint0hsaka@protonmail.com>
2023-03-20 20:07:44 +0100

    some changes

--- Src/main.c
+++ Src/main.c
@@ -1,5 +1,4 @@
 //Locate Peripheral base address
-
 #define PERIPH_BASE           (0x40000000UL)                    /*!< Peripheral base address */
 
 #define AHB1PERIPH_OFFSET     (0X20000UL)                       /*!< AHB1 peripheral offset */
@@ -25,18 +24,37 @@
 #define GPIOA_MODER_R         (*(volatile unsigned int*)(GPIOA_BASE + MODER_OFFSET))
 
 // Input data register
-#define IDR_OFFSET            (0x08h)
+#define IDR_OFFSET            (0x08UL)
 #define IDR_BASE              (GPIOA_BASE + IDR_OFFSET)
+#define GPIOA_IDR_R           (*(volatile unsigned int*)(GPIOA_BASE + IDR_OFFSET))
 
 // Output data register
 #define ODR_OFFSET            (0x0CUL)
-#define GPIOA_OD_R            (GPIOA_BASE + ODR_OFFSET)
+#define GPIOA_OD_R            (*(volatile unsigned int*)(GPIOA_BASE + ODR_OFFSET))
 
 // GPIOA pin 5
 #define GPIOA_PIN5            (1U<<5)
 #define LED_PIN               GPIOA_PIN5
 
+int main(void){
+    // Enabling clock acces to GPIOA
+    RCC_AHB1ENR_R |= GPIOAEN;
 
+    // Set PA5 as output
 
+    // Set bit 10 to 1
+    GPIOA_MODER_R |= (1U<<10);
+    // Set bit 11 to 0
+    GPIOA_MODER_R &= ~(1U<<11);
 
-
+    while(1){
+        // Read the value of PA0
+        if(GPIOA_IDR_R & 1U){
+            // Set PA5 to 1
+            GPIOA_OD_R |= LED_PIN;
+        }else{
+            // Set PA5 to 0
+            GPIOA_OD_R &= ~LED_PIN;
+        }
+    }
+}
